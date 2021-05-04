b050bbf3b96b68af7f74d7031d6d6a09c24972c6
empathy <empathyyiyiqi@gmail.com>
2021-05-04 20:20:40 +0800

    modify picture upload script

--- picUpload.sh
+++ picUpload.sh
@@ -1,1 +1,8 @@
-picgo upload
+#!/bin/bash
+res=`picgo upload | grep "https://empathy.oss-cn-beijing.aliyuncs.com/pic/"`
+if [ $res != "" ]; then
+	echo "![]($res)" | xclip -sel clip
+	notify-send "Img Uploaded"
+else
+	notify-send "Failed"
+fi
