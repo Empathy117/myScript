397b32a4379497b9da55308ab281d1f84f132fbd
empathy <empathyyiyiqi@gmail.com>
2021-05-09 11:59:46 +0800

    modify dbrun

--- dbrun.sh
+++ dbrun.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-if [ $# == 0 ] ##判断参数制是否存在
-then
-	exit ##不存在退出
-else
-	g++ -Wall -std=c++17 -x c++ -o debug -D QUICKRUN -D EDEBUG $1 && ./debug
-fi
+g++ -Wall -std=c++17 -o $1 $1.cpp && ./$1
