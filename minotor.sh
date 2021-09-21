dfc34edffb69c2cf0bbb3c02c0e27f591de39704
empathy <empathyyiyiqi@gmail.com>
2021-09-21 23:08:51 +0800

    update

--- minotor.sh
+++ minotor.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-xrandr --output HDMI-1-0 --mode 2560x1440 --rate 74.97 --pos 0x0 --rotate normal --output eDP1 --primary --mode 1920x1080 --rate 144.00 --pos 2560x360 --rotate normal 
+# xrandr --output HDMI-1-0 --mode 2560x1440 --rate 74.97 --pos 0x0 --rotate normal --output eDP1 --primary --mode 1920x1080 --rate 144.00 --pos 2560x360 --rotate normal 
 
+# xrandr --output HDMI-0 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP-0 --off --output DP-1 --off --output eDP-1-1 --mode 1920x1080 --pos 2560x360 --rotate normal --output DP-1-1 --off --output HDMI-1-1 --off
+xrandr --output HDMI-0 --primary --mode 2560x1440 --rate 74.97 --pos 0x0 --rotate normal --output DP-0 --off --output DP-1 --off --output eDP-1-1 --mode 1920x1080 --rate 144.00 --pos 2560x360 --rotate normal --output DP-1-1 --off --output HDMI-1-1 --off
